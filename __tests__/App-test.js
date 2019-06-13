50e31912c200ba6f89aec9d2bc5526f888044a05
jgam <jgam@alumni.nd.edu>
2019-06-13 18:33:25 +0900

    react Native TDD

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,20 +4,21 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
+import { Text } from 'react-native';
+import { shallow } from 'enzyme';
 
-console.log('test pased');
-// Note: test renderer must be required after react-native.
-/*
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
-*/
 describe('Jest', ()=> {
-  it('is it working?', () => {
-      const a = 1;
-      expect(a + 1).toBe(2)
-  })
+    it('is it working?', () => {
+        const a = 1;
+        expect(a + 1).toBe(2)
+    })
+})
+
+describe('Enzyme', () => {
+    it('is it working?', () => {
+        const text = 'some text';
+        const wrapper = shallow(<Text>{text}</Text>);
+        expect(wrapper.text()).toBe(text);
+    })
 })
